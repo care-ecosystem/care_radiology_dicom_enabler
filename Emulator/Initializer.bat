c3dd232073c42f4bc77aadf1bc95d8e04184de42
Muhammed Sawad <sawadmuhammedpm@gmail.com>
2026-07-17 10:37:15 +0530

    Added prompt option for entering mysql password while running bat (#24)

--- Emulator/Initializer.bat
+++ Emulator/Initializer.bat
@@ -5,7 +5,16 @@ echo ========================================
 
 set "DICOM_INSTALL_DIR=%~dp0Modality-Emulator-3.1.5.0\"
 set "DICOM_EXE_NAME=Modality-Emulator-3.1.5.0.msi"
-set "DICOM_MYSQL_PWD=inzin@123"
+
+set "DEFAULT_DICOM_MYSQL_PWD=care"
+
+echo.
+set /p DICOM_MYSQL_PWD=Enter MySQL password [Press Enter for default: %DEFAULT_DICOM_MYSQL_PWD%]: 
+
+if "%DICOM_MYSQL_PWD%"=="" set "DICOM_MYSQL_PWD=%DEFAULT_DICOM_MYSQL_PWD%"
+
+echo Using MySQL password: %DICOM_MYSQL_PWD%
+echo.
 
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Initializer.ps1" %*
 
